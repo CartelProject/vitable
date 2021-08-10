9ed27859cf3961ac1cc5ed10c6c188458e0799f2
Gagan Malvi <malvi@styxproject.ml>
2021-08-10 08:12:42 +0530

    VITable: cleanup and implement support for the new API response type.
    Signed-off-by: Gagan Malvi <malvi@styxproject.ml>

--- vitable.nim
+++ vitable.nim
@@ -2,22 +2,24 @@ import os, json, times, strutils, parseopt
 
 assert getHomeDir() == expandTilde("~")
 var path = getHomeDir()
-path.add(".tt.json")
+path.add(".vitable.json")
 let jsonNode = parseFile(path)
-var tt = jsonNode["timeTable"]
+var tt = jsonNode["Slots"]
 
 let daynow = now()
 var daytoday = toUpperAscii(daynow.format("ddd"))
 
 proc showTT() = 
-    for i in tt[daytoday]:
-        var
-            course = i["course"]
-            slot = i["slot"]
-            intime = i["inTime"]
-            outtime = i["outTime"]
-        echo ""
-        echo "Course: " & getStr(course)
-        echo "Timings: " & getStr(intime) & " - " & getStr(outtime)
-        echo "Slot: " & getStr(slot)
+    for i in tt:
+        if getStr(i["Day"]) == daytoday:
+            var
+                course = i["Course_Name"]
+                slot = i["Slot"]
+                intime = i["StartTime"]
+                outtime = i["EndTime"]
+            echo ""
+            echo "Course: " & getStr(course)
+            echo "Timings: " & getStr(intime) & " - " & getStr(outtime)
+            echo "Slot: " & getStr(slot)
 
+showTT()
